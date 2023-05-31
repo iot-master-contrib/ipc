ed28935e2ac4f71d08aa5c97d86df69d94fd9261
Jason <jason@zgwit.com>
2023-05-31 23:37:30 +0800

    更新文档

--- docs/ipc_docs.go
+++ docs/ipc_docs.go
@@ -3,7 +3,7 @@ package docs
 
 import "github.com/swaggo/swag"
 
-const docTemplate = `{
+const docTemplateipc = `{
     "schemes": {{ marshal .Schemes }},
     "swagger": "2.0",
     "info": {
@@ -464,18 +464,18 @@ const docTemplate = `{
     }
 }`
 
-// SwaggerInfo holds exported Swagger Info so clients can modify it
-var SwaggerInfo = &swag.Spec{
+// SwaggerInfoipc holds exported Swagger Info so clients can modify it
+var SwaggerInfoipc = &swag.Spec{
 	Version:          "1.0 版本",
 	Host:             "",
 	BasePath:         "/app/ipc/api/",
 	Schemes:          []string{},
 	Title:            "历史数据接口文档",
 	Description:      "API文档",
-	InfoInstanceName: "swagger",
-	SwaggerTemplate:  docTemplate,
+	InfoInstanceName: "ipc",
+	SwaggerTemplate:  docTemplateipc,
 }
 
 func init() {
-	swag.Register(SwaggerInfo.InstanceName(), SwaggerInfo)
+	swag.Register(SwaggerInfoipc.InstanceName(), SwaggerInfoipc)
 }
